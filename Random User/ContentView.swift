5a78da9e6330146da921e59a63e0eef457dcb4f2
Benjamin Sweett <benjamin.sweett@ceridian.com>
2020-09-30 12:19:19 -0400

    Template Project

---
+++ Random User/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Random User
+//
+//  Created by Sweett, Benjamin on 2020-09-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentViewPreviews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Random User/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  Random User
+//
+//  Created by Sweett, Benjamin on 2020-09-30.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
